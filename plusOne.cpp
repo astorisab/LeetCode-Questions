1b2ac2b9332f9f7558b54c017721cd89499e6088
astorisab <101530436+astorisab@users.noreply.github.com>
2022-03-18 12:52:26 -0400

    Update plusOne.cpp
    omit final for loop

--- plusOne.cpp
+++ plusOne.cpp
@@ -9,9 +9,6 @@ vector<int> plusOne(vector<int>& digits) {
         
         if(digits[0] == 0){
             digits[0] = 1;
-            for(int i = 1; i < digits.size(); ++i){
-                digits[i] = 0; 
-            }
             digits.push_back(0);
         }
         return digits;
