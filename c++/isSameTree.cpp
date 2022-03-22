ee55f52e666829591a1e554e27357c084d2baaa8
IA <isabellastor25@gmail.com>
2022-03-22 16:33:48 -0400

    file move

--- c++/isSameTree.cpp
+++ c++/isSameTree.cpp
@@ -1,8 +1,17 @@
+class Solution {
+public:
+
 bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p == nullptr && q == nullptr) return true;
-        if(p == nullptr || q == nullptr) return false;
+        if((p == nullptr && q != nullptr) || (p != nullptr && q == nullptr)) return false;
         
         if(p->val != q->val) return false;
-        return isSameTree(q->left, p->left) && isSameTree(q->right, p->right);
+        return isSameTree(q->left, p->right) && isSameTree(q->right, p->left);
             
     }
+        
+        
+    bool isSymmetric(TreeNode* root) {
+        return isSameTree(root->right, root->left);
+    }
+   };
