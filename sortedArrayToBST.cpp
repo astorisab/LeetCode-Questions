9de124fb8622e4bf072c20e53ae1b4360e9e45f6
astorisab <101530436+astorisab@users.noreply.github.com>
2022-03-22 17:17:00 -0400

    Create sortedArrayToBST.cpp

---
+++ sortedArrayToBST.cpp
@@ -0,0 +1,30 @@
+TreeNode* sortedArrayToBST(vector<int>& nums) {
+        
+//         recursive version
+        if(nums.size() == 0) return nullptr;
+        
+        if(nums.size() == 3){
+            TreeNode* start = new TreeNode(nums[1]);
+            start->left = new TreeNode(nums[0]);
+            start->right = new TreeNode(nums[2]);
+            return start;
+        }
+        if(nums.size() == 2){
+            TreeNode* start = new TreeNode(nums[1]);
+            start->left = new TreeNode(nums[0]);
+            return start;
+        }
+        
+        int mid = nums.size()/2;
+        
+        vector<int>numsLeft(nums.begin(), nums.begin() + mid);
+        vector<int>numsRight(nums.begin() + mid + 1, nums.end());
+        
+        TreeNode* root = new TreeNode(nums[mid]);
+            
+        root->left = sortedArrayToBST(numsLeft); 
+        root->right = sortedArrayToBST(numsRight);
+        
+        return root;
+
+    }
