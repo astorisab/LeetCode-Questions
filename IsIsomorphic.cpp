36539dcd64792ee97a3c2284d122d0fafad1fb74
astorisab <101530436+astorisab@users.noreply.github.com>
2022-04-22 16:45:15 -0400

    Update IsIsomorphic.cpp

--- IsIsomorphic.cpp
+++ IsIsomorphic.cpp
@@ -1,12 +1,15 @@
  bool isIsomorphic(string s, string t) {
         std::vector<int>ss;
         std::vector<int>tt;
+        
         ss.resize(256, -1);
         tt.resize(256, -1);
         
         
         int count = 0;
         for(auto x : s){
+         
+            //offset
             int sascii = 256 - int(x);
             int tascii = 256 - int(t[count]);
             if(ss[x] == -1 && tt[t[count]] == -1){
