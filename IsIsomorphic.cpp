bfc6d30f54893e9c97a668a068794059b8927825
astorisab <101530436+astorisab@users.noreply.github.com>
2022-04-22 16:57:17 -0400

    Update IsIsomorphic.cpp
    Offset change

--- IsIsomorphic.cpp
+++ IsIsomorphic.cpp
@@ -10,8 +10,8 @@
         for(auto x : s){
          
             //offset
-            int sascii = 256 - int(x);
-            int tascii = 256 - int(t[count]);
+            int sascii = 255 - int(x);
+            int tascii = 255 - int(t[count]);
             if(ss[x] == -1 && tt[t[count]] == -1){
                 ss[x] = tascii;
                 tt[t[count]] = sascii;
